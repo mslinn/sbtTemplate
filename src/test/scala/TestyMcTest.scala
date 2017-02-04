d14ca76538da7419714010b05da4f85babf4b323
Mike Slinn <mslinn@micronauticsresearch.com>
2017-02-04 03:20:19 -0800

    Added more ScalaTest sample tests

--- src/test/scala/TestyMcTest.scala
+++ src/test/scala/TestyMcTest.scala
@@ -1,23 +1,93 @@
-/* The person who associated a work with this deed has dedicated the work to the public domain by waiving all of his or
- * her rights to the work worldwide under copyright law, including all related and neighboring rights, to the extent allowed by law.
- *
- * You can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission.
- *
- * In no way are the patent or trademark rights of any person affected by CC0, nor are the rights that other persons may
- * have in the work or in how the work is used, such as publicity or privacy rights.
- * Unless expressly stated otherwise, the person who associated a work with this deed makes no warranties about the work,
- * and disclaims liability for all uses of the work, to the fullest extent permitted by applicable law.
- * When using or citing the work, you should not imply endorsement by the author or the affirmer.
- *
- * The full legal text is here: https://creativecommons.org/publicdomain/zero/1.0/legalcode */
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest._
+import org.scalatest.Matchers._
 
-import org.scalatest.{MustMatchers, WordSpec}
-
+@RunWith(classOf[JUnitRunner])
 class TestyMcTest extends WordSpec with MustMatchers {
-  "Tests" must {
-    "work" in {
-      1+2 === 3
-      ()
+  "The 'Hello world' string" should {
+    "contain 11 characters" in {
+      "Hello world".length === 11
+    }
+
+    "start with 'Hello'" in {
+      "Hello world" should startWith("Hello")
+    }
+
+    "end with 'world'" in {
+      "Hello world" should endWith("world")
+    }
+  }
+
+  "Strings" should {
+    val string =
+      """Thank you for your order.
+        |You enrolled in the best course ever!
+        |Go study and become successful.
+        |""".stripMargin
+
+    "compare normally" in {
+      string.length shouldBe 96
+      string should startWith("Thank you for your order")
+      string should include("the best course ever!")
+      string should endWith("successful.\n")
+    }
+
+    "compare with regexes" in {
+      string should fullyMatch regex "(?s)Thank .* best .*"
+      string should startWith regex "Thank .*"
+      string should include regex "best course .*"
+      string should endWith regex "successful.\n"
+    }
+  }
+
+  "OptionValue" should {
+    "work for Some values" in {
+      val option = Some(3)
+      option shouldBe defined
+      option.value shouldBe 3
+      option.value should be < 7
+      option should contain oneOf(3, 5, 7, 9)
+      List(3, 5, 7, 9) should contain(option.value)
+      option should not contain oneOf(7, 8, 9)
+      List(5, 7, 9) should not contain option.value
+    }
+
+    "work for None" in {
+      val option: Option[Int] = None
+      option shouldBe empty // the following are all equivalent:
+      option shouldEqual None
+      option shouldBe None
+      option should ===(None)
+    }
+  }
+
+  "EitherValue" should {
+    import org.scalatest.EitherValues._
+    val either: Either[String, Int] = Right(3)
+
+    "work for Right values" in {
+      either.right.value shouldBe 3
+      either shouldBe 'right
+      either should not be 'left
+    }
+  }
+
+  "Multiple predicates" should {
+    "combine" in {
+      val string =
+        """Thank you for your order.
+          |You enrolled in the best course ever!
+          |Go study and become successful.
+          |""".stripMargin
+      string should (
+        include("Thank you for your order") and
+          include("You enrolled in")
+        )
+      string should (
+        include("Thank you for your order") or
+          include("You enrolled in")
+        )
     }
   }
 }
