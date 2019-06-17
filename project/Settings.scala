a3f53aed115f0d6bbd09b65bd3744d26130e6930
Mike Slinn <mslinn@micronauticsresearch.com>
2019-06-17 09:24:29 -0700

    -

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,16 +1,46 @@
+import java.io.{BufferedReader, FileReader}
+
 object Settings {
+  import java.io.{BufferedWriter, File, FileWriter}
+
+  def using[A <: AutoCloseable, B](resource: A)
+                                  (block: A => B): B = {
+    try block(resource) finally resource.close()
+  }
+
+  def readLines(file: File): String =
+    using(new BufferedReader(new FileReader(file))) { _.readLine() }
+
+  def justForTravis(home: String): Unit = {
+    val configDir = new File(s"$home/.config/")
+    configDir.mkdir()
+    val hubFile = new File(configDir, "hub")
+    using(new BufferedWriter(new FileWriter(hubFile))) { bw =>
+      bw.write(
+        """- user: travis
+          |  oauth_token: 12345678901234567890
+          |  protocol: https
+          |""".stripMargin
+      )
+    }
+    ()
+  }
+
   lazy val gitHubId: String = try {
-        val hubConfigFilename = System.getProperty("user.home") + "/.config/hub"
-        scala.io.Source.fromFile(new java.io.File(hubConfigFilename))
-          .getLines
-          .filter(_.contains("user:"))
-          .toList
-          .headOption
-          .map(_.split(" ").slice(2, 3).mkString)
-          .mkString
-      } catch {
-        case ex: Exception =>
-          System.err.println(ex.getMessage)
-          sys.exit(-1)
-      }
+    val home = System.getProperty("user.home")
+    if (home=="/home/travis") justForTravis(home)
+    val hubConfigFile = new File(s"$home/.config/hub")
+    if (hubConfigFile.exists)
+      readLines(hubConfigFile)
+        .split("\n")
+        .find(_.contains("user:"))
+        .map(_.split(" ").slice(2, 3).mkString)
+        .mkString
+    else
+      "noGithubUserFound"
+  } catch {
+    case ex: java.io.IOException =>
+      System.err.println(s"project/Settings.scala error: ${ ex.getMessage }; ${ ex }")
+      sys.exit(-1)
+  }
 }
