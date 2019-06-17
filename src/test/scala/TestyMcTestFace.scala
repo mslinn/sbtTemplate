9b14ef6c58e0c543c7487ea74ca711613b28e922
Mike Slinn <mslinn@micronauticsresearch.com>
2019-06-17 10:52:28 -0700

    -

--- src/test/scala/TestyMcTestFace.scala
+++ src/test/scala/TestyMcTestFace.scala
@@ -1,7 +1,8 @@
-import org.scalatest._
 import org.scalatest.Matchers._
+import org.scalatest._
+import org.scalatest.wordspec.AnyWordSpec
 
-class TestyMcTestFace extends WordSpec with MustMatchers {
+class TestyMcTestFace extends AnyWordSpec with MustMatchers {
   "The 'Hello world' string" should {
     "contain 11 characters" in {
       "Hello world".length === 11
